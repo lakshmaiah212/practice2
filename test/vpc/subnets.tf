e706c86a4a6b325ff8dfe753a2fec06e89745c45
Your Name <you@example.com>
2019-10-14 14:06:41 +0530

    addedprivatesubnets

--- test/vpc/subnets.tf
+++ test/vpc/subnets.tf
@@ -27,3 +27,28 @@ resource "aws_route_table_association" "public_Subnet_association" {
   subnet_id      = "${element(aws_subnet.public_subnets.*.id, count.index)}"
   route_table_id = "${aws_route_table.publicsubnet_routetable.id}"
 }
+
+
+resource "aws_subnet" "private_subnets" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  vpc_id     = "${aws_vpc.test_vpc.id}"
+  cidr_block = "${cidrsubnet(var.vpc_cidr, 3, count.index+6)}"
+
+  tags = {
+    Name = "privatesubnets-${count.index}"
+  }
+}
+
+resource "aws_route_table" "privatesubnet_routetable" {
+  vpc_id = "${aws_vpc.test_vpc.id}"
+
+  tags = {
+    Name = "privatesubnet_route_table"
+  }
+}
+
+resource "aws_route_table_association" "private_Subnet_association" {
+  count = "${length(data.aws_availability_zones.available.names)}"
+  subnet_id      = "${element(aws_subnet.private_subnets.*.id, count.index)}"
+  route_table_id = "${aws_route_table.privatesubnet_routetable.id}"
+}
