0a5539cc10c138bea2127a0115486fb0612154fe
Your Name <you@example.com>
2019-10-14 16:50:42 +0530

    serverindex

--- test/ec2/server.tf
+++ test/ec2/server.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0b69ea66ff7391e80"
   instance_type = "t2.micro"
   key_name    = "endpoint"
-  subnet_id   =  "${vars.subnets_id}"
+  subnet_id   =  "${element(var.subnets_id,count.index)}"
   associate_public_ip_address = true
   vpc_security_group_ids = "${aws_security_group.ssh.id}"
 
