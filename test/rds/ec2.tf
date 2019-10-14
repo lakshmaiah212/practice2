d5704bba33561c528ede344095fd1948742c457b
Your Name <you@example.com>
2019-10-14 18:57:45 +0530

    addedtestinstance

---
+++ test/rds/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "mariadb-client" {
+  count = 1
+  ami           = "ami-0b69ea66ff7391e80"
+  instance_type = "t2.micro"
+  key_name    = "endpoint"
+  subnet_id   =  "${element(var.subnets_id,count.index)}"
+  associate_public_ip_address = true
+  vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
+
+  tags = {
+    Name = "mariadb_client"
+    project_name = "${var.project_name}"
+    project_env = "${var.project_env}"
+  }
+}
--- test/rds/rds-groups.tf
+++ test/rds/rds-groups.tf
@@ -40,3 +40,25 @@ resource "aws_security_group" "mysql" {
 
   }
 }
+
+
+resource "aws_security_group" "ssh" {
+  name        = "ssh_traffic"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${var.vpc_id}"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+
+  }
+}
