97a6073ce0a320fda34584cdf45c893e0bf495ab
Your Name <you@example.com>
2019-10-14 15:07:00 +0530

    deletedsubnets

--- test/vpc/subnets.tf
+++ test/vpc/subnets.tf
@@ -16,7 +16,6 @@ resource "aws_route_table" "publicsubnet_routetable" {
     gateway_id = "${aws_internet_gateway.igw.id}"
   }
 
-
   tags = {
     Name = "publicsubnet_route_table"
   }
@@ -26,29 +25,4 @@ resource "aws_route_table_association" "public_Subnet_association" {
   count = "${length(data.aws_availability_zones.available.names)}"
   subnet_id      = "${element(aws_subnet.public_subnets.*.id, count.index)}"
   route_table_id = "${aws_route_table.publicsubnet_routetable.id}"
-}
-
-
-resource "aws_subnet" "private_subnets" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-  vpc_id     = "${aws_vpc.test_vpc.id}"
-  cidr_block = "${cidrsubnet(var.vpc_cidr, 3, count.index+3)}"
-
-  tags = {
-    Name = "privatesubnets-${count.index}"
-  }
-}
-
-resource "aws_route_table" "privatesubnet_routetable" {
-  vpc_id = "${aws_vpc.test_vpc.id}"
-
-  tags = {
-    Name = "privatesubnet_route_table"
-  }
-}
-
-resource "aws_route_table_association" "private_Subnet_association" {
-  count = "${length(data.aws_availability_zones.available.names)}"
-  subnet_id      = "${element(aws_subnet.private_subnets.*.id, count.index)}"
-  route_table_id = "${aws_route_table.privatesubnet_routetable.id}"
 }
